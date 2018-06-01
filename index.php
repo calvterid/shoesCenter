d54a3f72eacc5b57f8f6d8063d1a6db51a1672d6
calvterid <calvterid@gmail.com>
2018-06-02 04:10:32 +0700

    Progress

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <title>Shoes Center</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+<body>
+    <div class="layout">
+        <div class="container" id="head">
+            <img src="img/head.jpg">
+        </div>
+    </div>
+</body>
+</html>
+
